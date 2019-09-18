124ef5fba7e9a13aed323242a840f9f090ff7380
Taras Chuiko <taraschuiko@gmail.com>
2019-09-18 15:44:47 +0300

    Added partial feature, improved pipes

--- gulpfile.js
+++ gulpfile.js
@@ -5,40 +5,42 @@ var imagemin = require('gulp-imagemin');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var htmlmin = require('gulp-htmlmin');
+var htmlpartial = require('gulp-html-partial');
 
-//html minify
-gulp.task('htmlmin', function() {
-	gulp.src('./src/*.html')
-		.pipe(htmlmin({collapseWhitespace: true}))
+//html partials and minify
+gulp.task('html', function () {
+	gulp.src(['./src/*.html', './src/*/*.html'])
+		.pipe(htmlpartial({ basePath: './src/partials/' }))
+		.pipe(htmlmin({ collapseWhitespace: true }))
 		.pipe(gulp.dest('./build/'))
 });
 
 // sass compile and minify
-gulp.task('style', function() {
+gulp.task('style', function () {
 	gulp.src('./src/style/*.+(scss|sass|css)')
 		.pipe(sass())
 		.pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
+		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest('./build/style/'))
 });
 
 // js compress
-gulp.task('uglify', function() {
+gulp.task('uglify', function () {
 	gulp.src('./src/js/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest('./build/js/'))
 });
 
 // image optimization
-gulp.task('imagemin' , function() {
+gulp.task('imagemin', function () {
 	gulp.src('./src/img/*')
 		.pipe(imagemin())
-        .pipe(gulp.dest('./build/img/'))
+		.pipe(gulp.dest('./build/img/'))
 });
 
 // watch
-gulp.task('watch', function() {
-	gulp.watch('./src/*.html', ['htmlmin']);
+gulp.task('watch', function () {
+	gulp.watch(['./src/*.html', './src/*/*.html'], ['html']);
 	gulp.watch('./src/style/*.+(scss|sass)', ['style']);
 	gulp.watch('./src/js/*.js', ['uglify']);
 	gulp.watch('./src/img/*', ['imagemin']);
